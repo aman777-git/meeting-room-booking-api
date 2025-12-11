865b2bb8e1f99cbc32bc67bf15ae636d50b3ca67
aman <amanmeshram9158@gmail.com>
2025-12-11 12:49:40 +0530

    Initial commit

---
+++ src/main/java/com/apibackend/meetingroombooking/MeetingroombookingApplication.java
@@ -0,0 +1,13 @@
+package com.apibackend.meetingroombooking;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MeetingroombookingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MeetingroombookingApplication.class, args);
+	}
+
+}
